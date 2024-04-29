3df62bb0c7ae2fd0af484390d2da5e26d824453d
Talha Nadeem <121732245+TalhaNadeem001@users.noreply.github.com>
2024-04-29 05:13:02 +0500

    Update config_example.py

--- src/config_example.py
+++ src/config_example.py
@@ -3,5 +3,5 @@
     Create config.py yourself with the configuration below with your own API keys
 """
 
-porcupine_access_key = "HRhemwEq3mFZjOSnrx9l3hx1Y4mL9noRCveVT1Y1midGlwfmS6/lRw=="
-gpt_key = "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"
+porcupine_access_key = ""
+gpt_key = ""
